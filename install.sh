eb68d15cda670a7efbf3e4717054dc26027c2f5a
Edward Yang <edward.yang6771@gmail.com>
2016-01-12 06:04:01 -0600

    Fix install script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-sudo pacman -S conky zsh neovim python2-neovim python3-neovim ctags python-twisted python2-twisted curl wget cmake base-devel clang sakura
+cd $DIR
+sudo pacman -S conky zsh neovim python2-neovim python-neovim ctags python-twisted python2-twisted curl wget cmake base-devel clang sakura docky intel-ucode
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sudo pip install argparse
 curl -sf https://raw.githubusercontent.com/brson/multirust/master/blastoff.sh | sh
@@ -12,6 +13,7 @@ mkdir -p "$HOME/.local/nvim"
 cp "init.vim" "$HOME/.local/nvim/init.vim"
 git clone "https://github.com/Valloric/YouCompleteMe" "$HOME/.local/nvim/plugged/YouCompleteMe"
 cd "$HOME/.local/nvim/plugged/YouCompleteMe"
-python2 ./install.sh --clang-complete --racer-complete
+git submodule update --init --recursive
+python2 ./install.py --clang-complete --racer-complete
 cd "$HOME"
 # Run :PlugInstall after completion in nvim
